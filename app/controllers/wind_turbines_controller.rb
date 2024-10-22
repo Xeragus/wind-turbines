4a181bb942a81e6999bdcfb87d6aebd4027cc7bf
Boban Sugareski <boban@actionvfx.com>
2024-10-22 10:41:00 +0200

    Add tailwind

---
+++ app/controllers/wind_turbines_controller.rb
@@ -0,0 +1,5 @@
+class RoomsController < ApplicationController
+  def index
+    @hehe = 'hihi'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :rooms do
     resources :messages
   end
+
+  resources :wind_turbines, only: [:index]
 end
